9fb1c07f31a9de73667d57a986921b196107dea1
Daniel Eriksson <635+daniel@users.noreply.github.com>
2018-11-17 16:58:53 +0100

    Render markdown

--- src/TV.js
+++ src/TV.js
@@ -1,11 +1,16 @@
 import React, { Component } from 'react';
 import './App.css';
+import ReactMarkdown from 'react-markdown/with-html';
+
+const markdown = `# Header
+Lorem Ipsum är en utfyllnadstext från tryck- och förlagsindustrin. Lorem ipsum har varit standard ända sedan 1500-talet, när en okänd boksättare tog att antal bokstäver och blandade dem för att göra ett provexemplar av en bok. Lorem ipsum har inte bara överlevt fem århundraden, utan även övergången till elektronisk typografi utan större förändringar. Det blev allmänt känt på 1960-talet i samband med lanseringen av Letraset-ark med avsnitt av Lorem Ipsum, och senare med mjukvaror som Aldus PageMaker.
+`
 
 class TV extends Component {
   render() {
     return (
       <div className="tv">
-        TV
+        <ReactMarkdown source={markdown} escapeHtml={false} />
       </div>
     );
   }
